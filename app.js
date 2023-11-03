aa339bdc9007ca6d1af0c708ead3bbb17ada0153
Mandip Adhikari <mandipadhikari105@gmail.com>
2023-11-03 06:07:57 +0000

    basic setup

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
 // Store for in-progress games. In production, you'd want to use a DB
 const activeGames = {};
 
+
+app.get('/', function (req,res){
+  res.send("The bot is alive. App ID= "+ process.env.APP_ID)
+})
+
 /**
  * Interactions endpoint URL where Discord will send HTTP requests
  */
@@ -52,8 +57,59 @@ app.post('/interactions', async function (req, res) {
         },
       });
     }
+
+    if (name === 'test') {
+      // Send a message into the channel where command was triggered from
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          // Fetches a random emoji to send from a helper function
+          content: 'hello world ' + getRandomEmoji(),
+        },
+      });
+    }
   }
 });
+
+if (type === InteractionType.MESSAGE_COMPONENT) {
+  // custom_id set in payload when sending message component
+  const componentId = data.custom_id;
+  
+    if (componentId.startsWith('accept_button_')) {
+      // get the associated game ID
+      const gameId = componentId.replace('accept_button_', '');
+      // Delete message with token in request body
+      const endpoint = `webhooks/${process.env.APP_ID}/${req.body.token}/messages/${req.body.message.id}`;
+      try {
+        await res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            // Fetches a random emoji to send from a helper function
+            content: 'What is your object of choice?',
+            // Indicates it'll be an ephemeral message
+            flags: InteractionResponseFlags.EPHEMERAL,
+            components: [
+              {
+                type: MessageComponentTypes.ACTION_ROW,
+                components: [
+                  {
+                    type: MessageComponentTypes.STRING_SELECT,
+                    // Append game ID
+                    custom_id: `select_choice_${gameId}`,
+                    options: getShuffledOptions(),
+                  },
+                ],
+              },
+            ],
+          },
+        });
+        // Delete previous message
+        await DiscordRequest(endpoint, { method: 'DELETE' });
+      } catch (err) {
+        console.error('Error sending message:', err);
+      }
+    }
+  }
 
 app.listen(PORT, () => {
   console.log('Listening on port', PORT);
